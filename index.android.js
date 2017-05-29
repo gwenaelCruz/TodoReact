1a6ef5e381cee08d0568defa60440422082225c7
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-05-29 17:00:25 +0200

    Quelques ajouts de packages et début affichage liste

--- index.android.js
+++ index.android.js
@@ -8,19 +8,41 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  ListView,
   Text,
   View
 } from 'react-native';
 
-export default class TodoReact extends Component {
+class ToolBar extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          TodoReact
         </Text>
       </View>
     );
+  }
+}
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows([
+         'Test1', 'Test2'
+      ])
+    };
+  }
+
+  render() {
+    <View>
+      <ListView
+          dataSource={ this.state.dataSource }
+          renderRow={ (rowData) => <Text>{rowData}</Text> }
+      />
+    </View>
   }
 }
 
@@ -28,9 +50,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
   }
 });
 
-AppRegistry.registerComponent('TodoReact', () => TodoReact);
+AppRegistry.registerComponent('TodoReact', () => ToolBar);
+AppRegistry.registerComponent('TodoReact', () => TodoList);
