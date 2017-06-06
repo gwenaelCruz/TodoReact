a2ee6c702b2ee9d26d1c455cf1021d736fcdcdfc
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-06-06 11:42:43 +0200

    Removed styles directory to put content in config/styles

--- app/config/styles.js
+++ app/config/styles.js
@@ -2,4 +2,25 @@
  * Created by DEVAPPLI on 31/05/2017.
  */
 
-// Global Colors
+/**
+ * Created by DEVAPPLI on 30/05/2017.
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        color: '#FFFFFF',
+        fontSize: 18
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
+    }
+
+
+});
+
+export default styles;
